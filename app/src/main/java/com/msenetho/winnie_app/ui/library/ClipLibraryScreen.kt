7113d55784d36085df0a85ef2ceadb10386430ba
Mearion <121697180+Mearion@users.noreply.github.com>
2026-07-17 21:34:38 -0500

    - Improved grid to be more visually appealing - Handled text wrapping and icon location

--- app/src/main/java/com/msenetho/winnie_app/ui/library/ClipLibraryScreen.kt
+++ app/src/main/java/com/msenetho/winnie_app/ui/library/ClipLibraryScreen.kt
@@ -1,13 +1,11 @@
 package com.msenetho.winnie_app.ui.library
 
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
